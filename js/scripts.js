63f0e56845b3d83541a683ed395ce018a6dfaf5e
erik1968 <59583004+erik1968@users.noreply.github.com>
2020-02-21 17:58:36 -0800

    Added Js move FlyingV & PlayMe on #Stage

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 console.log('OK');
 
-gsap.from('#PlayMe', { x: -300, duration: 2} );
+gsap.from('#PlayMe', { x: -500, duration: 3} );
+gsap.from( '#FlyingV', {
+// Move Play Me! and FlyingV to 'set the stage' for the animations 
+    x: -500,
+    opacity: 0,
+    duration: 3
+    
+  });
+
+// Next: on Click vibrates strings, send shockwave thru Google letters to the MegaStack amps
