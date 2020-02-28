865427f800256f37c3ba840e6b8eb52bcafc93cd
erik1968 <59583004+erik1968@users.noreply.github.com>
2020-02-27 20:17:13 -0800

    Added FlyingV Floating

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
 console.log('OK');
 
-gsap.from('#PlayMe', { x: -500, duration: 2.25} );
-gsap.from( '#FlyingV', {
-// Move Play Me! and FlyingV to 'set the stage' for the animations 
-    x: -500,
-    opacity: 0,
-    duration: 2.25
-    
-  });
-
-// Next: on Click vibrates strings, send shockwave thru Google letters to the MegaStack amps
+// Plan: on Click vibrates strings, send shockwave thru Google letters to the MegaStack amps
